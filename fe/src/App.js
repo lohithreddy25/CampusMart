64660a8a43abba3ab414149821a1693b4bdf90a2
lohithreddy25 <vempalli.lohithreddy@gmail.com>
2025-04-22 12:12:35 +0530

    added just comment

--- fe/src/App.js
+++ fe/src/App.js
@@ -5,10 +5,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ResetPassword from './pages/ResetPassword';
-import notifications from './pages/notifications';
-import query from './pages/query';
-
-
+// just checking 
 function App() {
   return (
     <Router>
@@ -18,8 +15,6 @@ function App() {
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/reset-password" component={ResetPassword} />
-        <Route path="/notifications" component={notifications} />
-        <Route path="/query" component={query} />
       </Switch>
     </Router>
   );
